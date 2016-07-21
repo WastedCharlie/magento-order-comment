c4885f99f6c20937df16a4dc29ba12d7f378073c
WastedCharlie <wasted_charlie@hotmail.com>
2016-07-21 12:45:29 +0100

    Adjust Save to cope with validation
    saveOrderComment function has to check the length validation on the comment.

--- app/code/community/Magemaven/OrderComment/Model/Observer.php
+++ app/code/community/Magemaven/OrderComment/Model/Observer.php
@@ -24,21 +24,41 @@ class Magemaven_OrderComment_Model_Observer extends Varien_Object
      *
      * @param $observer
      */
-    public function saveOrderComment($observer)
-    {
-        $orderComment = Mage::app()->getRequest()->getPost('ordercomment');
-        if (is_array($orderComment) && isset($orderComment['comment'])) {
-            $comment = trim($orderComment['comment']);
-            $comment = nl2br(Mage::helper('ordercomment')->stripTags($comment));
+	public function saveOrderComment($observer)
+	{
+		$orderComment = Mage::app()->getRequest()->getPost('ordercomment');
+		if (is_array($orderComment) && isset($orderComment['comment'])) {
+			$comment = trim($orderComment['comment']);
+			$comment = nl2br(Mage::helper('ordercomment')->stripTags($comment));
+			// Attempting some server-side validation to compensate for the absence of client-side.
+			$helper = Mage::helper('ordercomment');
+			$minLength = $helper->getMinimumCommentLength();
+			$maxLength = $helper->getMaximumCommentLength();
+			if($minLength > 0 || $maxLength > 0){
+				if(!Zend_Validate::is((string)$comment, 'StringLength', array('min' => $minLength, 'max' => $maxLength))){
+					if($minLength && $maxLength){
+						$exceptionMessage = $helper->__('Order comment should be between %s and %s characters.', $minLength, $maxLength);
+					} elseif($minLength > 0){
+						$exceptionMessage = $helper->__('Order comment should be at least %s characters.', $minLength);
+					} elseif($maxLength > 0){
+						$exceptionMessage = $helper->__('Order comment should be no longer than %s characters.', $maxLength);
+					}
+					/* Throwing an exception looks ugly, but should be handled by the try…catch around order->save.
+					* Using the Mage_core exception because only that and Mage_Payment_Model_Info are handled, if you 
+					* Mage_Payment_Model_Info will return to the Payment Information section. 
+					* */
+					throw Mage::exception('Mage_Core', $exceptionMessage, self::EXCEPTION_COMMENT_LENGTH); 
+				}
+			}
 
-            if (!empty($comment)) {
-                $order = $observer->getEvent()->getOrder(); 
-                $order->setCustomerNoteNotify(true);
-                $order->setCustomerNote($comment);
-                $this->_currentComment = $comment;
-            }
-        }
-    }
+			if (!empty($comment)) {
+				$order = $observer->getEvent()->getOrder(); 
+				$order->setCustomerNoteNotify(true);
+				$order->setCustomerNote($comment);
+				$this->_currentComment = $comment;
+			}
+		}
+	}
 
     /**
      * Show customer comment in 'My Account'
